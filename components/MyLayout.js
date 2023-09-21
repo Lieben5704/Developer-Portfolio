0e284ea0babc90d949d0d9b70a748b8e873e4cc8
Matthew Liebenberg <39401726+Lieben5704@users.noreply.github.com>
2023-09-21 10:12:39 +0200

    Update MyLayout.js

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -24,8 +24,19 @@ const Layout = (props) => {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous" // Use "crossOrigin" instead of "crossorigin"
+          crossOrigin="anonymous"
         />
+        
+        {/* Google Analytics script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B7NYJNRKZT"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-B7NYJNRKZT');
+          `}
+        </script>
       </Head>
       <div>
         {/* Header component */}
