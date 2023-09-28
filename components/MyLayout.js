24134ba534848c500e4ff05a4252ec51f41a2dbe
Matthew Liebenberg <39401726+Lieben5704@users.noreply.github.com>
2023-09-28 09:37:41 +0200

    Update MyLayout.js

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -3,14 +3,6 @@ import Header from "./Header";
 import { useEffect } from 'react';
 
 const Layout = (props) => {
-
-  useEffect(() => {
-    // Google Analytics script
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-VDXE8N8ND1');
-  }, []);
   
   return (
     <div>
@@ -22,7 +14,17 @@ const Layout = (props) => {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous"
         />
-        
+
+            <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VDXE8N8ND1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VDXE8N8ND1');
+</script>
+            
       </Head>
       <div>
         {/* Header component */}
